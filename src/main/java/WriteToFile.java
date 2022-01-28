114ee3e1c52c431dcf4a987e04df6fa64e15f2ea
Yurii19 <yurikolechkin@gmail.com>
2022-01-28 17:39:08 +0200

    refactor: in FileWrite class try - catch construction changed with try-with resources one

--- src/main/java/WriteToFile.java
+++ src/main/java/WriteToFile.java
@@ -5,8 +5,8 @@ import java.io.IOException;
 public class WriteToFile {
 
     public static void writeToFile(String logEntry) {
-        try {
-            FileWriter myWriter = new FileWriter("log.txt",true);
+        try( FileWriter myWriter = new FileWriter("log.txt",true)) {
+
             myWriter.write(logEntry+ "|+|");
             myWriter.close();
             System.out.println("Successfully wrote to the file.");
